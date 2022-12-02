9ce12c1b0ce8b68b6cff89c3cf6308a277988c81
CharmilaHruthikaTW <chamrilahruthika.s@thoughtworks.com>
2022-12-02 13:33:12 +0530

    [Keerthana|Hruthika] ADD. cache implementation

--- convo-commerce-orchestrator/src/main/java/com/thoughtworks/ondc/poc/pocwrapper/translation/TranslationService.java
+++ convo-commerce-orchestrator/src/main/java/com/thoughtworks/ondc/poc/pocwrapper/translation/TranslationService.java
@@ -3,11 +3,18 @@ package com.thoughtworks.ondc.poc.pocwrapper.translation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thoughtworks.ondc.poc.pocwrapper.cache.CacheHelper;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.Cache;
+
 import java.util.List;
 
 @Service
 public class TranslationService {
     private TranslationWebClient translationWebClient;
+
+    @Autowired
+    CacheHelper cacheHelper;
 
     @Autowired
     public TranslationService(TranslationWebClient translationWebClient) {
@@ -19,7 +26,19 @@ public class TranslationService {
     }
 
     public String translateFromIndicToEnglish(String text, String source) {
-        return translationWebClient.translate(text, source, "en").getTranslatedText();
+        Cache cache = cacheHelper.getAI4BharatCacheFile();
+        String response;
+        if(cache.get(text)==null) {
+            String translatedText = translationWebClient.translate(text, source, "en").getTranslatedText();
+            cache.put(new Element(text, translatedText));
+            response = translatedText;
+        }
+        else
+        {
+            Object object = cache.get(text).getObjectValue();
+            response = (String) object;
+        }
+        return response;
     }
 
     public List<String> translateFromEnglishToIndic(List<String> text, String target) {
