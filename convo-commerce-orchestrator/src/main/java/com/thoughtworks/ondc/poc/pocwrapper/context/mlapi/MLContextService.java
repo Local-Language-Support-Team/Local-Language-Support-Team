abec6e76f7370084a911b63615593686f2579964
CharmilaHruthikaTW <chamrilahruthika.s@thoughtworks.com>
2022-12-02 13:11:55 +0530

    Revert "[Keerthana|Hruthika] ADD. cache implementation"
    This reverts commit 65ee3e2bbc6b14e6da77ae90714cd3f13861ba37.

--- convo-commerce-orchestrator/src/main/java/com/thoughtworks/ondc/poc/pocwrapper/context/mlapi/MLContextService.java
+++ convo-commerce-orchestrator/src/main/java/com/thoughtworks/ondc/poc/pocwrapper/context/mlapi/MLContextService.java
@@ -2,20 +2,12 @@ package com.thoughtworks.ondc.poc.pocwrapper.context.mlapi;
 
 import com.thoughtworks.ondc.poc.pocwrapper.context.ContextResponse;
 import com.thoughtworks.ondc.poc.pocwrapper.context.ContextService;
-import com.thoughtworks.ondc.poc.pocwrapper.cache.CacheHelper;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.Element;
-
 
 @Service
 public class MLContextService implements ContextService {
-
-    @Autowired
-    CacheHelper cacheHelper;
 
     @Autowired
     ModelMapper modelMapper;
@@ -25,19 +17,8 @@ public class MLContextService implements ContextService {
 
     @Override
     public ContextResponse getContext(String contextInput) {
-        MLContextResponse response;
-        Cache cache = cacheHelper.getRasaCacheFile();
-        if(cache.get(contextInput)==null) {
-            MLContextResponse mlContextResponse = mlContextWebClient.getContext(contextInput);
-            cache.put(new Element(contextInput, mlContextResponse));
-            response = mlContextResponse;
-        }
-        else
-        {
-            Object object = cache.get(contextInput).getObjectValue();
-            response = (MLContextResponse)object;
-        }
-        return modelMapper.map(response, ContextResponse.class);
+        MLContextResponse mlContextResponse = mlContextWebClient.getContext(contextInput);
+        return modelMapper.map(mlContextResponse, ContextResponse.class);
     }
 
 }
